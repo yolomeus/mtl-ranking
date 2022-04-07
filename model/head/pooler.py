0ed2f1a363e77f697bbdbe4f69c7cf885f94178f
Fabian <fab.beringer@gmail.com>
2022-04-07 20:47:52 +0200

    add cls pooler

--- model/head/pooler.py
+++ model/head/pooler.py
@@ -47,6 +47,28 @@ class Average(Pooler):
         return x.mean(dim=1)
 
 
+class CLSToken(Pooler):
+    """Only get the CLS Token output which is expected to be the first token in the sequence. When pooling multiple
+    layers, the average of CLS token outputs will be returned.
+    """
+
+    def __init__(self, in_dim: int, out_dim: int, layers: List[int]):
+        super().__init__(in_dim, out_dim, layers)
+        assert in_dim == out_dim or out_dim is None
+
+    def forward(self, inputs):
+        # select CLS token for each layer
+        layer_outputs = [inputs[i][:, 0] for i in self.layers]
+
+        # only average if there are multiple cls tokens
+        if len(self.layers) > 1:
+            x = torch.stack(layer_outputs, -1).mean(-1)
+        else:
+            x = layer_outputs[0]
+
+        return x
+
+
 class SelfAttention(Pooler):
     """
 
